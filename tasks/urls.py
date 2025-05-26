50a683378a56f5f0311a727bd4bc882ec0670d1d
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-26 16:50:14 +0530

    Modifed to connect to the views such as update, delete and complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,7 @@ urlpatterns=[
     path("",views.index,name="index"),
     path("create/",views.create,name="create"),
     path("about/",views.about,name="about"),
+    path("update/<int:pk>",views.update,name="update"),
+    path("delete/<int:pk>",views.delete,name="delete"),
+    path("complete/<int:pk>",views.complete,name="complete")
 ]
