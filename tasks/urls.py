92de55fc9be173f40bd74d0a3de2839affb18f25
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-26 17:41:07 +0530

    Established url connection for the view completed_list

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,6 @@ urlpatterns=[
     path("about/",views.about,name="about"),
     path("update/<int:pk>",views.update,name="update"),
     path("delete/<int:pk>",views.delete,name="delete"),
-    path("complete/<int:pk>",views.complete,name="complete")
+    path("complete/<int:pk>",views.complete,name="complete"),
+    path("completed_tasks/",views.completed_list,name='completed_tasks'),
 ]
