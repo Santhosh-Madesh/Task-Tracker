de8112aec1fae92b5aeb10e29f057a82e164c54c
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-26 13:49:30 +0530

    Intialized models for the tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class TaskModel(models.Model):
+    title = models.CharField()
+    content = models.CharField()
+    deadline = models.DateField()
