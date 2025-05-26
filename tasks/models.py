67e193f5d92eb5ebf8f07329cd2ddcd4915501a7
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-26 16:49:15 +0530

    Modified Models to have a Boolean field and added a method that returns if a object/task is overdue or not

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 class TaskModel(models.Model):
     title = models.CharField()
     content = models.CharField()
     deadline = models.DateField()
+    completed = models.BooleanField(null=True)
+
+    def is_overdue(self):
+        return datetime.now()<self.deadline
